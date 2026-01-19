f2f3912391c4418952ad9e0aaee3167ac606844e
Dhanuja-29 <thanujanagarajan29@gmail.com>
2026-01-19 17:52:47 +0530

    Refine attendance evaluation conditions

--- src/AttendanceStatusEvaluator.java
+++ src/AttendanceStatusEvaluator.java
@@ -4,11 +4,11 @@ class AttendanceStatusEvaluator(){
   public static void main(String[] args){
    Scanner sc= new Scanner(System.in);
    int attendance=sc.nextInt();
-    if(attendance<60){
+    if((attendance >=0)&&(attendance<60)){
       System.out.println("Poor");
     }else if((attendance>=60) && (attendance<=85)){
       System.out.println("Satisfactory");
-    }else if(attendance>85){
+    }else if((attendance>85)&&(attendance<=100)){
       System.out.println("Excellent");
     }else{
       System.out.println("Enter valid input");

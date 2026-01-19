846749c5dfb24bde7bdfcf87d80f9a38f20ad42f
Dhanuja-29 <thanujanagarajan29@gmail.com>
2026-01-19 18:53:45 +0530

    Update AttendanceStatusEvaluator.java

--- src/AttendanceStatusEvaluator.java
+++ src/AttendanceStatusEvaluator.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.Scanner;
-public class AttendanceStatusEvaluator(){
+public class AttendanceStatusEvaluator{
   public static void main(String[] args){
    Scanner sc= new Scanner(System.in);
    int attendance=sc.nextInt();
